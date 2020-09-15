4ed7aa252c19fc05144f28f3405224a4ddce518c
RomaVintonyak <19roman23@gmail.com>
2020-09-15 13:53:53 +0300

    create intro slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-jQuery(document).ready(function(){
-  var tButton = $(".title");
-  tButton.on("click", show__messag);
-  function show__messag(){
-    confirm("its wery nice, click ok!");
-    for(var i = 0; i < 10; i++){
-      document.write("hello  world <br/>");
-    }
-  }
+jQuery(document).ready(function () {
+  "use script";
+  /*initialized slick slider to intro block*/
+  var introSlider = $("#introSlider");
+  introSlider.slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: false,
+    swipe: false,
+    touchMove: false,
+    focusOnSelect: false,
+    fade: true
+  });
 });
